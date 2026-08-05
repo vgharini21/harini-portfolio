477d7dedff383ce3a11f6bf724b08935cfa14c2a
vgharini <hv2179@nyu.edu>
2026-08-05 14:32:45 -0400

    Configure portfolio for root GitHub Pages domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/harini-portfolio' : '',
   images: {
     unoptimized: true,
   },
